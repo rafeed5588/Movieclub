b6d8e298766ad5e7bd7748751a320a7e47193485
NEGANTG <90514589+NEGANTG@users.noreply.github.com>
2022-01-23 14:33:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NEGANTG/cvilla-jan23.git /cvilla-jan23
+  git clone https://github.com/NEGANTG/cvilla-jan2.git /cvilla-jan2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /cvilla-jan23
+  git clone $UPSTREAM_REPO /cvilla-jan2
 fi
 cd /cvilla-jan23
 pip3 install -U -r requirements.txt

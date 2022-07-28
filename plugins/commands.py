4758078afc63da59d151b628f8ea18d96c37785b
NoobRider <62932698+NoobRider@users.noreply.github.com>
2022-07-28 11:57:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -88,7 +88,7 @@ async def start(client, message):
 
 👇 JOIN THIS CHANNEL & CLICK TRY AGAIN 👇
 
-[https://t.me/+rTmPUqK2CfA4M2Q1]__**""",
+[https://t.me/+rb5bloUCK-A2Nzhl]__**""",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )

809c191beb0cee1f4296be8c62492dc04e7f661e
Albert Einstein <moviezhood@gmail.com>
2022-11-19 10:33:21 +0530

    Done

--- plugins/channel.py
+++ plugins/channel.py
@@ -15,6 +15,6 @@ async def media(bot, message):
     else:
         return
 
-    media.file_type = file_type
+    media.file_type = file_type.value
     media.caption = message.caption
     await save_file(media)

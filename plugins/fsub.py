3b159accb4006a172eea63a475a5a0db59702815
NoobRider <62932698+NoobRider@users.noreply.github.com>
2023-01-27 23:30:35 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -117,7 +117,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
 
 👇 CLICK "REQUEST TO JOIN CHANNEL" THEN CLICK "TRY AGAIN" 👇
 
-[https://t.me/+LOa3b9OCctA0M2M1]__**"""
+[https://t.me/+9BPOd0xxaaBiYjU9]__**"""
 
         buttons = [
             [InlineKeyboardButton("📢 Request to Join Channel 📢", url=invite_link)],
